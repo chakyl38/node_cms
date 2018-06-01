b4da72e25a668031ed4bde50da655ae2b6dc9d60
Kyle Chapman <kylechapman09@gmail.com>
2018-06-01 17:18:07 -0500

    added routes to ui nav

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ app.get('/about', (req, res)=>{
     res.render('home/about');
 });
 
+app.get('/login', (req, res)=>{
+    res.render('home/login');
+});
+
+app.get('/register', (req, res)=>{
+    res.render('home/register');
+});
+
+
 app.listen(5000, ()=>{
     console.log(`Listening on port 5000`);
 });
