c875fb500642abe29aaebde9bf3a62459b70b608
Kyle Chapman <kylechapman09@gmail.com>
2018-06-03 16:14:53 -0500

    Displaying post in admin dashboard

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -8,7 +8,9 @@ router.all('/*', (req, res, next) =>{
 });
 
 router.get('/', (req, res)=>{
-    res.send('It Works');
+    Post.find({}).then(posts =>{
+        res.render('admin/posts', {posts: posts});
+    });
 });
 
 router.get('/create', (req, res)=>{
