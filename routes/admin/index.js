7ad65a47445f466eb60c40f32b140fc3c82fbb21
Kyle Chapman <kylechapman09@gmail.com>
2018-06-03 23:36:12 -0500

    added faker for testing

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const faker   = require('faker');
+const Post    = require('../../models/Post');
 const router = express.Router();
 
 router.all('/*', (req, res, next) =>{
@@ -10,6 +12,19 @@ router.get('/', (req, res)=>{
     res.render('admin/index');
 });
 
+router.post('/generate-fake-posts', (req,res)=>{
+    for(let i = 0; i < req.body.amount; i++){
+        let post = new Post();
+        post.title = faker.name.title();
+        post.status = 'public';
+        post.allowComments = faker.random.boolean();
+        post.body = faker.lorem.sentences();
 
+        post.save(function(err){
+            if(err) throw err;
+        });
+    };
+    res.redirect('/admin/posts');
+});
 
 module.exports = router;
